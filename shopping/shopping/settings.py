894489d6b20e61a8f7a3f3a7fe3ee7c30795debc
Prajyot1603 <prajyotgharat16@gmail.com>
2024-03-12 11:57:40 +0530

    commit message

--- shopping/shopping/settings.py
+++ shopping/shopping/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles_build','static')
+    os.path.join(BASE_DIR,'static')
 ]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
